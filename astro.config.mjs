0da48e4be2664b28fe988715976e95755e67095e
C0RR1T <74185591+C0RR1T@users.noreply.github.com>
2026-09-06 16:48:33 +0200

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,26 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig, fontProviders } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig, fontProviders } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-	fonts: [
-		{
-			provider: fontProviders.local(),
-			name: 'Atkinson',
-			cssVariable: '--font-atkinson',
-			fallbacks: ['sans-serif'],
-			options: {
-				variants: [
-					{
-						src: ['./src/assets/fonts/atkinson-regular.woff'],
-						weight: 400,
-						style: 'normal',
-						display: 'swap',
-					},
-					{
-						src: ['./src/assets/fonts/atkinson-bold.woff'],
-						weight: 700,
-						style: 'normal',
-						display: 'swap',
-					},
-				],
-			},
-		},
-	],
+  site: "https://blog.c0rr1t.dev",
+  integrations: [mdx(), sitemap()],
+  output: "static",
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: "Space Mono",
+      cssVariable: "--font-space-mono",
+      fallbacks: ["sans-serif"],
+    },
+  ],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
